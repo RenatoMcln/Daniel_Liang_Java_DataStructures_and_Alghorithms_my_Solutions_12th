e5190c6b359ef69ecb48eb44cf4a16e3bda68bd4
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-17 02:41:10 -0300

    Introduction to Computers, Programs, and Java.

--- Chapter1/Exercise1_1.java
+++ Chapter1/Exercise1_1.java
@@ -1,5 +1,7 @@
 package Chapter1;
-
+/**
+ * Class Exercise1_1 displays three messages on the console
+ */
 public class Exercise1_1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
--- Chapter1/Exercise1_2.java
+++ Chapter1/Exercise1_2.java
@@ -1,5 +1,7 @@
 package Chapter1;
-
+/**
+ * class Exercise1_2 displays five messages on the console
+ */
 public class Exercise1_2 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
