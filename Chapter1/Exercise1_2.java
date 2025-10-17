81c679c6b338eaebc775ad6d9b1ff506ca4661f9
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-17 02:26:31 -0300

    Introduction to Computers, Programs, and Java.

---
+++ Chapter1/Exercise1_2.java
@@ -0,0 +1,11 @@
+package problems.chapter1;
+
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
