06e2ffd8c14ff69b74847d29ddc51209f2012bab
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-18 03:15:37 -0300

    Introduction to Computers, Programs, and Java.

--- Chapter1/Exercise1_3.java
+++ Chapter1/Exercise1_3.java
@@ -1,6 +1,10 @@
 package Chapter1;
 
 public class Exercise1_3 {
+    /**
+     * This method displays 4 lines whose together bild the word "JAVA"
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("    J     A     V     V     A   ");
         System.out.println("    J    A A     V   V     A A  ");
