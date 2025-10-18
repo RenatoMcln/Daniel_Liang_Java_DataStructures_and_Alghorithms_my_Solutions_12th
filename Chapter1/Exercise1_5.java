877f0b506bf542465876822b2ab702a3b5408931
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-18 15:24:16 -0300

    Introduction to Computers, Programs, and Java.

--- Chapter1/Exercise1_5.java
+++ Chapter1/Exercise1_5.java
@@ -1,5 +1,4 @@
 package Chapter1;
-
 /**
  * 1.5 (Compute expressions) Write a program that displays the result of
  * ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5)
---
+++ Chapter1/Exercise1_6.java
@@ -0,0 +1,12 @@
+package Chapter1;
+/**
+ * 1.6 (Summation of a series) Write a program that displays the result of
+ * 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+ */
+//This could be solved in one "System.out.print" by using concatenation
+public class Exercise1_6 {
+    public static void main(String[] args) {
+        System.out.print("The result of\n1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 is = ");
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
