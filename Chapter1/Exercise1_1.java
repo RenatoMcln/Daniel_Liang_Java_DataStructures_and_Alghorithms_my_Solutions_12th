e52dc36267f1aa56a87f7c66c53324edf7525c86
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-18 14:21:51 -0300

    Introduction to Computers, Programs, and Java.

--- Chapter1/Exercise1_1.java
+++ Chapter1/Exercise1_1.java
@@ -3,6 +3,7 @@ package Chapter1;
  * 1.1 (Display three messages) Write a program that displays Welcome to Java,
  * Welcome to Computer Science, and Programming is fun.
  */
+//This could be solved in one line by using "\n" as line break, but that would make the line too long for readability.
 public class Exercise1_1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
--- Chapter1/Exercise1_2.java
+++ Chapter1/Exercise1_2.java
@@ -4,6 +4,7 @@ package Chapter1;
  * times.
  */
 //Here I didn't take "Loops" class yet.
+//This could be solved in one line by using "\n" as line break, but that would make the line too long for readability.
 public class Exercise1_2 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
