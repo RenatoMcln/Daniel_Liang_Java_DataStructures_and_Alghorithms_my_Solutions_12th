ca16f38fec14696bceae319f0cb17f7422a04e88
RenatoMcln <renatodeveloper17@gmail.com>
2025-10-18 15:01:46 -0300

    Introduction to Computers, Programs, and Java.

---
+++ Chapter1/Exercise1_5.java
@@ -0,0 +1,13 @@
+package Chapter1;
+
+/**
+ * 1.5 (Compute expressions) Write a program that displays the result of
+ * ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5)
+ */
+//Java already has operator precedence rules, but it's better to use brackets to avoid ambiguity.
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.print("The result of\n((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5) is = ");
+        System.out.println( ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5) );
+    }
+}
